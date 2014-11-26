86704610de61c75b6f147508fddae761f55403c7
SebastianBoyd <Family@chrisbosmacbook.att.net>
2014-11-25 17:02:38 -0800

    Added morse code dictionary
    added python dictionary containing translations from letters to morse
    code

--- translate.py
+++ translate.py
@@ -1,11 +1,51 @@
 '''Helper functions for morse code translation.'''
-__author__ = 'insert name here'
+__author__ = 'Sebastian Boyd'
 
 
 def text_to_morse(words):
-    '''Translate a list of words into morse code.
+        '''Translate a list of words into morse code.
 
-    @param words: A list of Alphanumeric strings.
-    @returns: A list of lists of strings containing . and -
-    '''
-    return [['-.-.-', '---', '..-'], ['-..', '-..', '...', '---']]
+        @param words: A list of Alphanumeric strings.
+        @returns: A list of lists of strings containing . and -
+        '''
+        print morse_alphabet
+        return [['-.-.-', '---', '..-'], ['-..', '-..', '...', '---']]
+
+'''Morse code dictionary definitions'''
+morse_alphabet = {}
+morse_alphabet['a'] = '.-'
+morse_alphabet['b'] = '-...'
+morse_alphabet['c'] = '-.-.'
+morse_alphabet['d'] = '-..'
+morse_alphabet['e'] = '.'
+morse_alphabet['f'] = '..-.'
+morse_alphabet['g'] = '--.'
+morse_alphabet['h'] = '....'
+morse_alphabet['i'] = '..'
+morse_alphabet['j'] = '.---'
+morse_alphabet['k'] = '-.-'
+morse_alphabet['l'] = '.-..'
+morse_alphabet['m'] = '--'
+morse_alphabet['n'] = '-.'
+morse_alphabet['o'] = '---'
+morse_alphabet['p'] = '.--.'
+morse_alphabet['q'] = '--.-'
+morse_alphabet['r'] = '.-.'
+morse_alphabet['s'] = '...'
+morse_alphabet['t'] = '-'
+morse_alphabet['u'] = '..-'
+morse_alphabet['v'] = '...-'
+morse_alphabet['w'] = '.--'
+morse_alphabet['x'] = '-..-'
+morse_alphabet['y'] = '-.--'
+morse_alphabet['z'] = '--..'
+morse_alphabet['1'] = '.----'
+morse_alphabet['2'] = '..---'
+morse_alphabet['3'] = '...--'
+morse_alphabet['4'] = '....-'
+morse_alphabet['5'] = '.....'
+morse_alphabet['6'] = '-....'
+morse_alphabet['7'] = '--...'
+morse_alphabet['8'] = '---..'
+morse_alphabet['9'] = '----.'
+morse_alphabet['0'] = '-----'
