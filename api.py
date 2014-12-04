cbb98052ce56fd8a90d289b7eaf0a982ecfc506a
SebastianBoyd <Family@Chris-Boyds-MacBook.local>
2014-12-03 18:04:36 -0800

    Added api method (still doesn't work)

--- api.py
+++ api.py
@@ -18,7 +18,9 @@ class MorseTranslateAPI(remote.Service):
     '''The morse translation API.'''
     @endpoints.method(MorsecodeRequest,
                   MorsecodeResponse,
-                  name='morstranslate.morsecode', path='texttomorse')
-
+                  name='morsetranslate.morsecode', path='texttomorse')
+    def morsecode(self, request):
+        data = text_to_morse(request)
+	return data
 
 application = endpoints.api_server([MorseTranslateAPI])
