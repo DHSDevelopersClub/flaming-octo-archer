04712f0c77940bfef8a45388467f26eb933fc9e1
Max Starr <manmon42@gmail.com>
2014-12-06 23:18:13 -0800

    Added other menu option

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var menu = true;
-translator = document.getElementById('translator');
+var translator = document.getElementById('translator');
 var translateExample = function () {
-    input = document.getElementById("text-input").value;
+    var input = document.getElementById("text-input").value;
     translator.input = input;
     translator.updateOut();
-}
+};
 var elem = document.querySelector('.mainPage');
+var elem2 = document.querySelector('.menu');
+var elem3 = document.querySelector('.pageCover');
 
-var showMenu = new Animation(elem, [
+var showMenu1 = new Animation(elem, [
     {
         left: '0px'
             },
@@ -20,7 +22,8 @@ var showMenu = new Animation(elem, [
     iterations: 1
 });
 
-var hideMenu = new Animation(elem, [
+
+var hideMenu1 = new Animation(elem, [
     {
         left: '200px'
             },
@@ -32,11 +35,75 @@ var hideMenu = new Animation(elem, [
     duration: 200,
     iterations: 1
 });
+
+var showMenu2 = new AnimationGroup([
+    new Animation(elem2, [
+        {
+            left: '-200px'
+            },
+        {
+            left: '0px'
+            }
+            ], {
+        fill: 'forwards',
+        duration: 200,
+        iterations: 1
+    }),
+
+    new Animation(elem3, [
+        {
+            opacity: '0'
+        },
+        {
+            opacity: '0.5'
+        }
+        ], {
+        fill: 'forwards',
+        duration: 200,
+        iterations: 1
+    })
+]);
+
+var hideMenu2 = new AnimationGroup([
+
+    new Animation(elem2, [
+        { left: '0px'},
+        { left: '-200px'}
+    ], {
+    fill: 'forwards',
+    duration: 200,
+    iterations: 1
+    }),
+
+    new Animation(elem3, [
+        {opacity: '0.5'},
+        {opacity: '0'}
+    ], {
+    fill: 'forwards',
+    duration: 200,
+    iteration: 1
+    })
+]);
+
 var play = function () {
     if (menu) {
-        var player = document.timeline.play(showMenu);
+        var player = document.timeline.play(showMenu1);
     } else {
-        var player = document.timeline.play(hideMenu);
+        var player = document.timeline.play(hideMenu1);
     }
     menu = !menu;
+}
+
+var show = function () {
+    elem3.style.display = 'block';
+    elem2.style.boxShadow = "10px 0px 20px 0.1px #3b3b3b";
+    var player = document.timeline.play(showMenu2);
+}
+var hide = function () {
+    var player = document.timeline.play(hideMenu2);
+
+    setTimeout(function(){
+        elem3.style.display = 'none';
+        elem2.style.boxShadow = 'none';
+    }, 200);
 }
